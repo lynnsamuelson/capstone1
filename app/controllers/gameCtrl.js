6c5a798dcdd79854d287dcd596ea99c5d88cab4a
Lynn Samuelson <lynn.samuelson@gmail.com>
2015-09-15 11:38:07 -0500

    made drag and drop with 2 images anchored to different boxes & updataed images

--- app/controllers/gameCtrl.js
+++ app/controllers/gameCtrl.js
@@ -23,53 +23,52 @@ define([
     //console.log("game", game);
     function preload() {
       this.scale.pageAlignHorizontally = true;
-      game.load.image("background", "images/beaker.jpg");
-      game.load.spritesheet("Cl", "images/Cl.jpg", 60, 59);
-      game.load.spritesheet("Na", "images/Na.jpg", 60, 59);
-      game.load.spritesheet("NaCl", "images/NaCl.jpg", 60, 59);
+      game.load.image("background", "images/beaker.png");
+      game.load.image("Cl", "images/ClBox.jpg", 60, 59);
+      game.load.spritesheet("Na", "images/NaPurple.jpg", 60, 59);
+      game.load.spritesheet("NaCl", "images/NaClBox.jpg", 60, 59);
+      game.load.spritesheet("box", "images/emptyBox.jpg", 60, 59);
+      game.load.spritesheet("boxNa", "images/emptyBox.jpg", 60, 59);
+
 
     }
     function create() {
+      //this.game.state.start.('preload');
       this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
       game.add.sprite(game.world.centerX, game.world.centerY, 'background');
 
-        
 
-       //static box
-      this.NaCl = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'Cl');
-      this.NaCl.anchor.setTo(3.5, 4);
+       //static NaCl box
+      this.NaCl = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'NaCl');
+      this.NaCl.anchor.setTo(3, 4);
       
-      //static box
-      this.Cl = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'Cl');
-      this.Cl.anchor.setTo(0.5, 4);
-
-      //movable Cl box
-      this.game.physics.arcade.enable(this.Cl);
-      this.ClCopy = this.game.add.sprite(this.game.world.centerX, 200, this.Cl.key, this.Cl.frame);
-      this.ClCopy.anchor.x = 0.5;
-
-      this.game.physics.arcade.enable(this.ClCopy);
-      this.ClCopy.inputEnabled = true;
-      this.ClCopy.input.enableDrag();
-      this.ClCopy.originalPosition = this.ClCopy.position.clone();
+      //static Cl box
+      this.box = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'box');
+      this.box.anchor.setTo(0.5, 4);
 
       //static Na box
-      this.Na = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'Na');
-      this.Na.anchor.setTo(-1, 4);
+      this.boxNa = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'boxNa');
+      this.boxNa.anchor.setTo(-1, 4);
+
+      //movable Cl box
+      this.Cl = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'Cl');
+      this.Cl.anchor.setTo(.5, 2);
+      this.game.physics.arcade.enable(this.box);
+      this.game.physics.arcade.enable(this.Cl);
+      this.Cl.inputEnabled = true;
+      this.Cl.input.enableDrag();
+      this.Cl.originalPosition = this.box.position.clone();
+
 
       //movable Na box
+      this.Na = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'Na');
+      this.Na.anchor.setTo(-1, 2.5);
+      this.game.physics.arcade.enable(this.boxNa);
       this.game.physics.arcade.enable(this.Na);
-      this.NaCopy = this.game.add.sprite(this.game.world.centerX, 200, this.Na.key, this.Na.frame);
-      this.NaCopy.anchor.x = -1;
-      this.game.physics.arcade.enable(this.NaCopy);
-      this.NaCopy.inputEnabled = true;
-      this.NaCopy.input.enableDrag();
-      this.NaCopy.originalPosition = this.NaCopy.position.clone();
-
-
-
-
+      this.Na.inputEnabled = true;
+      this.Na.input.enableDrag();
+      this.Na.originalPosition = this.boxNa.position.clone();
 
       
       var _this = this;
@@ -79,22 +78,26 @@ define([
                                                       endSprite, 
                                                       function() {
                                                         currentSprite.input.draggable = false; 
-                                                        currentSprite.position.copyFrom(endSprite.position); 
-                                                        currentSprite.anchor.setTo(endSprite.anchor.x, endSprite.anchor.y); 
+                                                        console.log("drug")
+                                                        //currentSprite.position.copyFrom(endSprite.position); 
+                                                        //currentSprite.anchor.setTo(endSprite.anchor.x, endSprite.anchor.y);
+                                                        //currentSprite = endSprite;
+                                                        //endSprite.anchor.setTo(currentSprite.originalPosition.x, currentSprite.originalPosition.y);
+                                                        //endSprite.anchor.setTo(0, 0);
+
                                                       } );
         if ( !tophat ) {
           currentSprite.position.copyFrom(currentSprite.originalPosition);
         }
       }
 
-      this.ClCopy.events.onDragStop.add(function(currentSprite){
-        stopDrag(currentSprite, this.Cl);
-        console.log("match");
+      this.Cl.events.onDragStop.add(function(currentSprite){
+        stopDrag(currentSprite, this.box);
       }, this);
 
-       this.NaCopy.events.onDragStop.add(function(currentSprite){
-        stopDrag(currentSprite, this.Na);
-        console.log("match");
+      //anchors the Na box to the correct position in the equation
+       this.Na.events.onDragStop.add(function(currentSprite){
+        stopDrag(currentSprite, this.boxNa);
       }, this);
       
     }
