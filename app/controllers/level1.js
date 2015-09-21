5789675208c9ef032eb1901d426716bbb024ad26
Lynn Samuelson <lynn.samuelson@gmail.com>
2015-09-21 11:54:25 -0500

    non-answer boxes drag working and started logic to make those boxes random

--- app/controllers/level1.js
+++ app/controllers/level1.js
@@ -22,7 +22,10 @@ define([
       var cationBox;
       var anionBox;
       var formula;
+      var equation;
+      var variables;
       var formulaArray = [];
+      var display = [];
 
       //console.log("game", game);
 
@@ -49,77 +52,136 @@ define([
 
       //static arrow box
       this.arrows = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'arrows');
-      this.arrows.anchor.setTo(1.8, 5); 
+      this.arrows.anchor.setTo(2, 5); 
 
       //static Water box
       this.water = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'water');
-      this.water.anchor.setTo(2.3, 5);
+      this.water.anchor.setTo(2.2, 5);
 
-      equation = formulaArray.splice(Math.floor(Math.random()*formulaArray.length),1);;
-
+      equation = formulaArray.splice(Math.floor(Math.random()*formulaArray.length),1);
       anion = equation[0].anion;
       cation = equation[0].cation;
       formula = equation[0].formula;
       //console.log("anion", anion);
         
-        //static box for the formula
-        this.formula = this.game.add.sprite(this.game.world.centerX, this.game.world.height, formula);
-        this.formula.anchor.setTo(2.5, 4);
+      //static box for the formula
+      this.formula = this.game.add.sprite(this.game.world.centerX, this.game.world.height, formula);
+      this.formula.anchor.setTo(2.7, 4);
 
-        //static empty box anchored to anion
-        this.anionBox = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'anionBox');
-        this.anionBox.anchor.setTo(-1, 4);
+      //static empty box anchored to anion
+      this.anionBox = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'anionBox');
+      this.anionBox.anchor.setTo(-1, 4);
 
-        //movable anion box
-        this.anion = this.game.add.sprite(this.game.world.centerX, this.game.world.height, anion);
-        this.anion.anchor.setTo(-1, 2);
-        this.game.physics.arcade.enable(this.anionBox);
-        this.game.physics.arcade.enable(this.anion);
-        this.anion.inputEnabled = true;
-        this.anion.input.enableDrag();
-        this.anion.originalPosition = this.anionBox.position.clone();
+      //movable anion box
+      this.anion = this.game.add.sprite(this.game.world.centerX, this.game.world.height, anion);
+      this.anion.anchor.setTo(-0.7, 2.3);
+      this.game.physics.arcade.enable(this.anionBox);
+      this.game.physics.arcade.enable(this.anion);
+      this.anion.inputEnabled = true;
+      this.anion.input.enableDrag();
+      this.anion.originalPosition = this.anionBox.position.clone();
 
-        // //static empty box anchored to cation
-        this.cationBox = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'cationBox');
-        this.cationBox.anchor.setTo(0.8, 4);
+      // //static empty box anchored to cation
+      this.cationBox = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'cationBox');
+      this.cationBox.anchor.setTo(0.5, 4);
 
-        // //movable anion box
-        this.cation = this.game.add.sprite(this.game.world.centerX, this.game.world.height, cation);
-        this.cation.anchor.setTo(0.8, 2);
-        this.game.physics.arcade.enable(this.cationBox);
-        this.game.physics.arcade.enable(this.cation);
-        this.cation.inputEnabled = true;
-        this.cation.input.enableDrag();
-        this.cation.originalPosition = this.cationBox.position.clone();
+      // //movable anion box
+      this.cation = this.game.add.sprite(this.game.world.centerX, this.game.world.height, cation);
+      this.cation.anchor.setTo(0.4, 2.3);
+      this.game.physics.arcade.enable(this.cationBox);
+      this.game.physics.arcade.enable(this.cation);
+      this.cation.inputEnabled = true;
+      this.cation.input.enableDrag();
+      this.cation.originalPosition = this.cationBox.position.clone();
 
-        //add other sprites to the page
-        this.Cl = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'Cl');
-        this.Cl.anchor.setTo(0.8, 0);
-
-
-
-        var _this = this;
-        function stopDrag(currentSprite, endSprite){
-          var tophat = _this.game.physics.arcade.overlap(currentSprite, 
-                                                        endSprite, 
-                                                        function() {
-                                                          currentSprite.input.draggable = false; 
-                                                        } );
-          if ( !tophat ) {
-            currentSprite.position.copyFrom(currentSprite.originalPosition);
-          }
+      
+      for (var i = 0; i < 3; i++) {
+        variables = formulaArray.splice(Math.floor(Math.random()*formulaArray.length),1);
+        if (variables[0].anion !== display[i]) {
+          display.push(variables[0].anion);
+          display.push(variables[0].cation);
         }
+      }
+      console.log("display", display);
 
-         this.anion.events.onDragStop.add(function(currentSprite){
-          stopDrag(currentSprite, this.anionBox);
-        }, this);
 
-         this.cation.events.onDragStop.add(function(currentSprite){
-          stopDrag(currentSprite, this.cationBox);
-        }, this);
-          //console.log(this.cationBox.position)
-         
-     }
+
+      //add other sprites to the page
+      this.Cl = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'Cl');
+      this.Cl.anchor.setTo(1.5, 2.3);
+      this.game.physics.arcade.enable(this.Cl);
+      this.game.physics.arcade.enable(this.Cl);
+      this.Cl.inputEnabled = true;
+      this.Cl.input.enableDrag();
+      this.spriteOrigPos = {x:0, y:0};
+
+      this.Br = this.game.add.sprite(this.game.world.centerX, this.game.world.height, 'Br');
+      this.Br.anchor.setTo(2.7, 2.3);
+      this.game.physics.arcade.enable(this.Br);
+      this.game.physics.arcade.enable(this.Br);
+      this.Br.inputEnabled = true;
+      this.Br.input.enableDrag();
+      this.spriteOrigPos = {x:0, y:0};
+
+
+
+      
+      var _this = this;
+      function bounceBack(currentSprite, oPos){
+        if ( currentSprite.position.x !== oPos.x && currentSprite.position.y !== oPos.y ) {
+          currentSprite.position.x = oPos.x;
+          currentSprite.position.y = oPos.y;
+        }
+      }
+
+      this.Cl.events.onDragStart.add(function(sprite) {
+        console.log("Firing drag start now");
+        this.spriteOrigPos.x = sprite.position.x;
+        this.spriteOrigPos.y = sprite.position.y;
+        console.log(this.spriteOrigPos.x, this.spriteOrigPos.y);
+      }, this);
+
+      this.Cl.events.onDragStop.add(function(currentSprite){
+        console.log(this.spriteOrigPos.x, this.spriteOrigPos.y);
+        bounceBack(currentSprite, this.spriteOrigPos);
+      }, this);
+
+      this.Br.events.onDragStart.add(function(sprite) {
+        console.log("Firing drag start now");
+        this.spriteOrigPos.x = sprite.position.x;
+        this.spriteOrigPos.y = sprite.position.y;
+        console.log(this.spriteOrigPos.x, this.spriteOrigPos.y);
+      }, this);
+
+      this.Br.events.onDragStop.add(function(currentSprite){
+        console.log(this.spriteOrigPos.x, this.spriteOrigPos.y);
+        bounceBack(currentSprite, this.spriteOrigPos);
+      }, this);
+
+
+
+      function stopDrag(currentSprite, endSprite){
+        var inCorrectPosition = _this.game.physics.arcade.overlap(currentSprite, 
+          endSprite, 
+          function() {
+            currentSprite.input.draggable = false; 
+          } );
+
+        if ( !inCorrectPosition ) {
+          currentSprite.position.copyFrom(currentSprite.originalPosition);
+        }
+      }
+
+       this.anion.events.onDragStop.add(function(currentSprite){
+        stopDrag(currentSprite, this.anionBox);
+      }, this);
+
+       this.cation.events.onDragStop.add(function(currentSprite){
+        stopDrag(currentSprite, this.cationBox);
+      }, this);
+        //console.log(this.cationBox.position)
+       
+   }
 
     function update() {
       if (this.cation.input.draggable === false && this.anion.input.draggable === false) {
